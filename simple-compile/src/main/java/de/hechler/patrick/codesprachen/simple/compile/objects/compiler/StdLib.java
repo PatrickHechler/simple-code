cfd4970f2475b98f88428f7105d48887742d6503
pat <pat@Jerry>
2023-05-13 09:25:52 +0200

    update

--- simple-compile/src/main/java/de/hechler/patrick/codesprachen/simple/compile/objects/compiler/StdLib.java
+++ simple-compile/src/main/java/de/hechler/patrick/codesprachen/simple/compile/objects/compiler/StdLib.java
@@ -5,35 +5,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import de.hechler.patrick.codesprachen.primitive.core.utils.PrimAsmConstants;
+import de.hechler.patrick.codesprachen.simple.symbol.objects.SimpleVariable;
+import de.hechler.patrick.codesprachen.simple.symbol.objects.SimpleVariable.SimpleOffsetVariable;
 import de.hechler.patrick.codesprachen.simple.symbol.objects.types.SimpleFuncType;
+import de.hechler.patrick.codesprachen.simple.symbol.objects.types.SimpleType;
+import de.hechler.patrick.codesprachen.simple.symbol.objects.types.SimpleTypePointer;
 
 @SuppressWarnings("javadoc")
 public class StdLib {
 	
-	public record Func(SimpleFuncType signature, int paramStartReg, int resultStartReg) {
-		
-		public Func {
-			if (paramStartReg < PrimAsmConstants.X_ADD || resultStartReg < PrimAsmConstants.X_ADD) {
-				throw new AssertionError();
-			}
-			long bc = signature.byteCount();
-			if (paramStartReg < SimpleCompiler.MIN_COMPILER_REGISTER - bc || resultStartReg < SimpleCompiler.MIN_COMPILER_REGISTER - bc) {
-				throw new AssertionError();
-			}
-		}
-		
-	}
-	
-	public static Map<String, Func> ALL_INTERRUPTS = allInts();
-	
+	public static Map<String, SimpleFuncType> ALL_INTERRUPTS = allInts();
 	
 	// GENERATED-CODE-START
-	private static Map<String, Func> allInts() {
-		Map<String, Func> res = new HashMap<>();
-		new SimpleFuncType(List.of(), List.of());
+	private static Map<String, SimpleFuncType> allInts() {
+		Map<String, SimpleFuncType> res = new HashMap<>();
+		res.put("name", new SimpleFuncType(List.of(), List.of()));
 		return Collections.unmodifiableMap(res);
 	}
 	// GENERATED-CODE-END
+	
+	private static SimpleOffsetVariable sv(SimpleType type, int deapth, String name) {
+		if (deapth < 0) throw new AssertionError();
+		while (deapth > 0) type = new SimpleTypePointer(type);
+		return new SimpleVariable.SimpleOffsetVariable(type, name);
+	}
 	
 }
