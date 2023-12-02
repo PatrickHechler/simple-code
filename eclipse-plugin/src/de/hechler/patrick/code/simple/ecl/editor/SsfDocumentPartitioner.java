dfcfa6f5675ec775af0fd781e18833053f541188
pat <pat@Jerry>
2023-12-02 12:19:58 +0100

    update

--- eclipse-plugin/src/de/hechler/patrick/code/simple/ecl/editor/SsfDocumentPartitioner.java
+++ eclipse-plugin/src/de/hechler/patrick/code/simple/ecl/editor/SsfDocumentPartitioner.java
@@ -3,7 +3,6 @@ package de.hechler.patrick.code.simple.ecl.editor;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
 
